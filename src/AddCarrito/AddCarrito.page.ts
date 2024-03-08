8717388b103f93e0accdd362a214ac4ca76a5ff3
LUISRZCN <lfrodriguez5924@misena.edu.co>
2024-03-08 12:35:09 -0500

    agregar al carrito

--- src/AddCarrito/AddCarrito.page.ts
+++ src/AddCarrito/AddCarrito.page.ts
@@ -1,6 +1,5 @@
 import { Page, PageContext, pageProvider } from "@testing/wdio-page-objects";
 
-
 const selectors = {
   optionlogin: `.flex.pv2.items-center`,
   email: `input[placeholder='Escribe tu correo electrónico']`,
@@ -10,7 +9,8 @@ const selectors = {
   categorias: `//img[@alt='alt text']`,
   tecnologia: `(//div[contains(text(),'Tecnología')])[1]`,
   smartphone: `//a[normalize-space()='Smartphones']`,
-  btn_agregar: `//p[normalize-space()='Agregar']`,
+  btn_agregar: `(//button[contains(@type,'button')])[5]`,
+  //btn_agregar: `//p[normalize-space()='Agregar']`,
   verificacion_carrito: `//h3[normalize-space()='Carrito']`,
   lista_productos: `.vtex-search-result-3-x-galleryItem`,
   // selectores del modal
@@ -21,32 +21,35 @@ const selectors = {
   modal_ciudad: `//div[contains(text(),'Selecciona una ciudad')]`,
   modal_direccion: `//input[@id='autocomplete']`,
   btn_modal_confirmar: `//div[contains(text(),'Confirmar')]`,
-  btn_envio: `div[class='w-100 flex justify-center items-center']`
-}
+  amount: `input[type="tel"].vtex-numeric-stepper__input`,
+  btn_envio: `div[class='w-100 flex justify-center items-center']`,
+};
 
 @PageContext({
-  path: '/',
-  wrapper: 'body'//Nodo Padre
+  path: "/",
+  wrapper: "body", //Nodo Padre
 })
-
 export class AddProducto extends Page {
-
   // funcion del modal
-  enterDataModal(modal_departamento: string, modal_ciudad: string, modal_direccion: string) {
+  enterDataModal(
+    modal_departamento: string,
+    modal_ciudad: string,
+    modal_direccion: string
+  ) {
     $(selectors.btn_envio).click();
     $(selectors.modal_departamento_select).click();
     //Recorro la palabra y activo el evento de cada letra
     for (let i = 0; i < modal_departamento.length - 1; i++) {
       browser.keys(modal_departamento[i].toLocaleLowerCase());
     }
-    browser.keys('\uE007'); //Enter
+    browser.keys("\uE007"); //Enter
 
-    $(selectors.modal_ciudad).click()
+    $(selectors.modal_ciudad).click();
 
     for (let i = 0; i < modal_ciudad.length - 1; i++) {
       browser.keys(modal_ciudad[i].toLocaleLowerCase());
     }
-    browser.keys('\uE007'); //Enter
+    browser.keys("\uE007"); //Enter
 
     $(selectors.modal_direccion).click();
     $(selectors.modal_direccion).setValue(modal_direccion);
@@ -56,48 +59,57 @@ export class AddProducto extends Page {
 
   // Da click en el menu de categoria para desplegarlo
   clickCategoria() {
-    $(selectors.categorias).click(),
-      browser.pause(5000)
+    $(selectors.categorias).click(), browser.pause(5000);
   }
-  // Da click en la categoria y despliega un menu tipo overmouse 
+  // Da click en la categoria y despliega un menu tipo overmouse
   clickTecnologia() {
-    $(selectors.tecnologia).click(),
-      browser.pause(5000)
+    $(selectors.tecnologia).click(), browser.pause(5000);
   }
   //Da click en la subcategoria para entrar a la vista de dicha subcategoria
   clickSmartphones() {
-    $(selectors.smartphone).click(),
-      browser.pause(5000)
+    $(selectors.smartphone).click(), browser.pause(5000);
   }
 
-
   selectRandomProducts() {
-    const productoLista = $$(selectors.lista_productos); // Obtener todos los productos
-    const randomProductIndexes = this.generateRandomIndexes(productoLista.length, 3); // Generar 3 índices aleatorios
-    browser.pause(5000)
+    for (let i = 0; i < 3; i++) {
+      // Obtener todos los productos
+      const lista_productos = $$(selectors.lista_productos);
 
-    // Seleccionar aleatoriamente 3 productos
-    for (const index of randomProductIndexes) {
-      const product = productoLista[index];
-      product.$(selectors.btn_agregar).click(); // Agregar producto al carrito
+      // Seleccionar un producto aleatorio
+      const randomIndex = Math.floor(Math.random() * lista_productos.length);
+      const randomProduct = lista_productos[randomIndex];
+
+      randomProduct.scrollIntoView();
+
+      /*const headerHeight = 100;
+      const productPosition = randomProduct.getLocation("y") - headerHeight;
+
+      // hacer un scroll a una posicion visible
+      browser.execute((position) => {
+        window.scrollTo(0, position);
+      }, productPosition);
+*/
+      // Encontrar el botón de agregar asociado al producto y hacer clic en él
+      const btn_agregar = randomProduct.$(selectors.btn_agregar);
+      btn_agregar.click();
+      browser.pause(5000);
+
+      function obtenerNumeroAleatorio(): number {
+        // Genera un número aleatorio entre 1 y 5
+        const numeroAleatorio = Math.floor(Math.random() * 5) + 1;
+        return numeroAleatorio;
+      }
+
+      const numeroAleatorio = obtenerNumeroAleatorio();
+
+      $(selectors.amount).click(),
+        browser.keys('\uE017');
+      $(selectors.amount).setValue(numeroAleatorio),
+        browser.pause(5000)
     }
-    browser.pause(5000)
 
     // Ir al carrito de compras
     $(selectors.verificacion_carrito).click();
-    browser.pause(5000)
-  }
-
-  // Función para generar índices aleatorios únicos
-  generateRandomIndexes(max: number, count: number): number[] {
-    const indexes = [];
-    while (indexes.length < count) {
-      const randomIndex = Math.floor(Math.random() * max);
-      if (!indexes.includes(randomIndex)) {
-        indexes.push(randomIndex);
-      }
-    }
-    return indexes;
-
+    browser.pause(5000);
   }
 }
